348a24f9513e1256ea5b8150829ca418e91a1936
andrey-lawyer <andrey-1@ukr.net>
2024-01-09 11:54:07 +0200

    fix error

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -4,7 +4,8 @@ import { ICardsProps } from "@/types/typeProps";
 export const Cards: React.FC<ICardsProps> = ({ accommodationsFilter }) => {
   return (
     <ul className="p-3 bg-gray-200 flex flex-col gap-6 overflow-y-auto max-h-screen">
-      {accommodationsFilter.length > 0 &&
+      {accommodationsFilter &&
+        accommodationsFilter.length > 0 &&
         accommodationsFilter.map((el) => (
           <li key={el.id} className="  flex flex-col gap-2">
             <Image
